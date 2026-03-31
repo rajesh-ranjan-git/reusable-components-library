74a51ad3929db167d802f7761ecf45177e3ba540
Rajesh Ranjan <rajeshranjan8271@gmail.com>
2026-03-31 17:53:44 +0530

    fixed theme toggle

--- client/src/components/theme/themeToggle.tsx
+++ client/src/components/theme/themeToggle.tsx
@@ -4,14 +4,14 @@ import { themeConfig } from "@/config/common.config";
 import { useAppStore } from "@/store/store";
 
 const ThemeToggle = () => {
-  const switchTheme = useAppStore((state) => state.switchTheme);
-  const setSwitchTheme = useAppStore((state) => state.setSwitchTheme);
+  const activeTheme = useAppStore((state) => state.activeTheme);
+  const setActiveTheme = useAppStore((state) => state.setActiveTheme);
 
   const handleSwitchTheme = () => {
-    if (switchTheme === themeConfig.dark) {
-      setSwitchTheme(themeConfig.light);
+    if (activeTheme === themeConfig.dark) {
+      setActiveTheme(themeConfig.light);
     } else {
-      setSwitchTheme(themeConfig.dark);
+      setActiveTheme(themeConfig.dark);
     }
   };
 
@@ -20,7 +20,7 @@ const ThemeToggle = () => {
       <input
         type="checkbox"
         id="theme-toggle"
-        checked={switchTheme === themeConfig.dark ? true : false}
+        checked={activeTheme === themeConfig.dark ? true : false}
         className="peer hidden w-0 h-0"
         onChange={handleSwitchTheme}
       />
@@ -30,7 +30,7 @@ const ThemeToggle = () => {
       >
         <svg
           className={`top-1 left-1 z-50 absolute ${
-            switchTheme === themeConfig.dark ? "fill-[#7e7e7e]" : "fill-white"
+            activeTheme === themeConfig.dark ? "fill-[#7e7e7e]" : "fill-white"
           } w-4 transition-all duration-500`}
           viewBox="0 0 24 24"
           fill="currentColor"
@@ -39,7 +39,7 @@ const ThemeToggle = () => {
           <path
             d="M12 3V4M12 20V21M4 12H3M6.31412 6.31412L5.5 5.5M17.6859 6.31412L18.5 5.5M6.31412 17.69L5.5 18.5001M17.6859 17.69L18.5 18.5001M21 12H20M16 12C16 14.2091 14.2091 16 12 16C9.79086 16 8 14.2091 8 12C8 9.79086 9.79086 8 12 8C14.2091 8 16 9.79086 16 12Z"
             className={`${
-              switchTheme === themeConfig.dark
+              activeTheme === themeConfig.dark
                 ? "stroke-[#7e7e7e]"
                 : "stroke-white"
             } transition-all duration-500`}
@@ -51,7 +51,7 @@ const ThemeToggle = () => {
         </svg>
         <svg
           className={`top-1 left-11 z-50 absolute ${
-            switchTheme === themeConfig.dark ? "fill-white" : "fill-[#7e7e7e]"
+            activeTheme === themeConfig.dark ? "fill-white" : "fill-[#7e7e7e]"
           } w-4 transition-all duration-500`}
           viewBox="0 0 24 24"
           fill="currentColor"
@@ -60,7 +60,7 @@ const ThemeToggle = () => {
           <path
             d="M3.32031 11.6835C3.32031 16.6541 7.34975 20.6835 12.3203 20.6835C16.1075 20.6835 19.3483 18.3443 20.6768 15.032C19.6402 15.4486 18.5059 15.6834 17.3203 15.6834C12.3497 15.6834 8.32031 11.654 8.32031 6.68342C8.32031 5.50338 8.55165 4.36259 8.96453 3.32996C5.65605 4.66028 3.32031 7.89912 3.32031 11.6835Z"
             className={`${
-              switchTheme === themeConfig.dark
+              activeTheme === themeConfig.dark
                 ? "stroke-white"
                 : "stroke-[#7e7e7e]"
             } transition-all duration-500`}
--- client/src/store/store.tsx
+++ client/src/store/store.tsx
@@ -1,15 +1,11 @@
 import { Dispatch, SetStateAction } from "react";
 import { create } from "zustand";
 import { themeConfig } from "@/config/common.config";
-import { ContextMenuTypes, SheetTypes, ThemeTypes } from "@/types/types";
+import { ThemeTypes } from "@/types/types";
 
 type AppState = {
   activeTheme: ThemeTypes;
   setActiveTheme: Dispatch<SetStateAction<ThemeTypes>>;
-  activeSheet: SheetTypes;
-  setActiveSheet: Dispatch<SetStateAction<SheetTypes>>;
-  activeContextMenu: ContextMenuTypes;
-  setActiveContextMenu: Dispatch<SetStateAction<ContextMenuTypes>>;
 };
 
 export const useAppStore = create<AppState>((set) => ({
@@ -20,23 +16,5 @@ export const useAppStore = create<AppState>((set) => ({
         typeof themeOrUpdater === "function"
           ? themeOrUpdater(state.activeTheme)
           : themeOrUpdater,
-    })),
-
-  activeSheet: null,
-  setActiveSheet: (sheetOrUpdater) =>
-    set((state) => ({
-      activeSheet:
-        typeof sheetOrUpdater === "function"
-          ? sheetOrUpdater(state.activeSheet)
-          : sheetOrUpdater,
-    })),
-
-  activeContextMenu: null,
-  setActiveContextMenu: (contextMenuOrUpdater) =>
-    set((state) => ({
-      activeContextMenu:
-        typeof contextMenuOrUpdater === "function"
-          ? contextMenuOrUpdater(state.activeContextMenu)
-          : contextMenuOrUpdater,
     })),
 }));
